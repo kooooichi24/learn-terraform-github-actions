3bf07d1a25fbf76c7b08ab8939ee75464c76686a
kooooichi24 <ex.kouichi1224@gmail.com>
2023-11-20 22:20:01 +0900

    fix

--- dev/main.tf
+++ dev/main.tf
@@ -1,5 +1,5 @@
 # Resource Group
 resource "azurerm_resource_group" "example" {
-  name     = "learn-terraform-github-actions-rg-dev"
+  name     = "learn-terraform-github-actions-1-rg-dev"
   location = "japaneast"
 }
