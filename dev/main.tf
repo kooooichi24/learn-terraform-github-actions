594766bba2672f86adcb39e2b369a6fc81fd0557
kooooichi24 <ex.kouichi1224@gmail.com>
2023-11-22 14:32:23 +0900

    fix

--- dev/main.tf
+++ dev/main.tf
@@ -1,6 +1,6 @@
-# module "azure_bot" {
-#   source = "../modules/azure-bot"
+module "azure_bot" {
+  source = "../modules/azure-bot"
 
-#   product_name = "koichi-furukawa-example-1"
-#   environment  = "dev"
-# }
+  product_name = "koichi-furukawa-example-1"
+  environment  = "dev"
+}
